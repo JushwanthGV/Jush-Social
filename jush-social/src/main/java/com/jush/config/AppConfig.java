a6c07b255f42c3b8efdd581a00a8b1643f379c97
Jushwanth <76085349+JushwanthGV@users.noreply.github.com>
2024-04-03 00:58:58 +0530

    making a jwt token generator and check validation for user

--- jush-social/src/main/java/com/jush/config/AppConfig.java
+++ jush-social/src/main/java/com/jush/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfAuthenticationStrategy;
 
 @Configuration
@@ -22,7 +23,7 @@ public class AppConfig {
 		.authorizeHttpRequests(Authorize -> Authorize
 				.requestMatchers("/api/**").authenticated()
 				.anyRequest().permitAll())
-		
+		.addFilterBefore(new jwtValidator(), BasicAuthenticationFilter.class)
 		.csrf(csrf -> csrf.disable());
 		
 		
---
+++ jush-social/src/main/java/com/jush/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.jush.config;
+
+public class JwtConstant {
+
+	public static String JWT_HEADER="Authorization";
+	public static String SECRET_KEY="sajdasndjnklsadlsajsadnl";
+	
+}
